9333432875b90dc4ee008d44a5871753dc739ff9
CarstenDR2006 <100208@glr.nl>
2024-06-17 12:16:38 +0200

    light text

--- js/light.js
+++ js/light.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const thumbnails = document.querySelectorAll('.thumbnail');
     const popup = document.getElementById('popup');
     const popupImg = document.getElementById('popupImg');
+    const popupText = document.getElementById('popupText');
     const closeBtn = document.getElementById('close');
 
     thumbnails.forEach(thumbnail => {
         thumbnail.addEventListener('click', function () {
             popup.style.display = 'flex';
             popupImg.src = this.src;
+            popupText.innerText = this.getAttribute('data-text');
         });
     });
 
@@ -17,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     popup.addEventListener('click', function (e) {
-        if (e.target !== popupImg) {
+        if (e.target !== popupImg && e.target !== popupText) {
             popup.style.display = 'none';
         }
     });
